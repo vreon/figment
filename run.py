abec764efdf3b8f9d2a001fe833a6cbbaa616837
Jesse Dubay <jesse@thefortytwo.net>
2011-08-10 21:40:46 -0700

    Add setup.py and proper entry point.

--- run.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import schema
-
-if __name__ == '__main__':
-    schema.play()
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'schema',
+    version = '0.0.1',
+    author = 'Jesse Dubay',
+    author_email = 'jesse@thefortytwo.net',
+    description = (
+        'An interactive, text-based MUD engine based on '
+        'an entity-component system.'
+    ),
+    license = 'MIT',
+    keywords = 'game engine mud',
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Games/Entertainment :: Multi-User Dungeons (MUD)",
+        "License :: OSI Approved :: MIT License",
+    ],
+    entry_points={
+        'console_scripts': [
+            'schema = schema:play',
+        ],
+    }
+)
