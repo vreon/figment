660a4e56d8aadc38a4619db78b20e4a108278a37
Jesse Dubay <jesse@thefortytwo.net>
2013-02-03 01:15:04 -0800

    ugly workaround for yield+return in admin aspect

--- example/aspects/admin.py
+++ example/aspects/admin.py
@@ -1,39 +1,55 @@
 from schema import Aspect, action, jug
 
 class Admin(Aspect):
-    @staticmethod
-    def validate(event):
+    @action(r'^snapshot$')
+    def snapshot(event):
         if not event.actor.has_aspect(Admin):
             event.actor.tell("You're unable to do that.")
             return
 
         yield 'before'
-        return not event.prevented
-
-    @action(r'^snapshot$')
-    def snapshot(event):
-        if Admin.validate(event):
+        if not event.prevented:
             event.actor.tell('Saving snapshot.')
             event.actor.zone.save_snapshot()
 
     @action(r'^crash$')
     def crash(event):
-        if Admin.validate(event):
+        if not event.actor.has_aspect(Admin):
+            event.actor.tell("You're unable to do that.")
+            return
+
+        yield 'before'
+        if not event.prevented:
             raise RuntimeError('Craaaaash')
 
     @action(r'^restart$')
     def restart(event):
-        if Admin.validate(event):
+        if not event.actor.has_aspect(Admin):
+            event.actor.tell("You're unable to do that.")
+            return
+
+        yield 'before'
+        if not event.prevented:
             event.actor.tell('Restarting.')
             event.actor.zone.restart()
 
     @action(r'^halt$')
     def halt(event):
-        if Admin.validate(event):
+        if not event.actor.has_aspect(Admin):
+            event.actor.tell("You're unable to do that.")
+            return
+
+        yield 'before'
+        if not event.prevented:
             event.actor.tell('Shutting down.')
             event.actor.zone.stop()
 
     @action(r'^ping$')
     def ping(event):
-        if Admin.validate(event):
+        if not event.actor.has_aspect(Admin):
+            event.actor.tell("You're unable to do that.")
+            return
+
+        yield 'before'
+        if not event.prevented:
             event.actor.tell('Pong!')
