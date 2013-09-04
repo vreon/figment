a28edb145cc3a97738e016e64c4ac3d8296c498a
Jesse Dubay <jesse@thefortytwo.net>
2013-09-04 15:24:19 -0700

    Move example into figment-examples repo.

--- example/aspects/__init__.py
+++
@@ -1,9 +0,0 @@
-from positioned import Positioned
-from usable import Usable
-from dark import Dark
-from admin import Admin
-from emotes import Emotes
-from psychic import Psychic
-from sticky_blob import StickyBlob
-from fauna import Bird, Pest, ShoosPests
-from wandering import Wandering
--- example/aspects/psychic.py
+++
@@ -1,8 +0,0 @@
-from figment import Aspect, Event, before
-from . import Positioned
-
-class Psychic(Aspect):
-    @before(Positioned.say)
-    def prepeat(self, event):
-        if self.entity.has_aspect(Positioned):
-            self.entity.perform(Positioned.say, message=event.message)
