1ff30a008a4b3f46dbf540165a58db324bb70966
Jesse Dubay <jesse@thefortytwo.net>
2014-11-22 01:10:01 -0800

    Simplify construction of CLI parser

--- figment/cli.py
+++ figment/cli.py
@@ -79,76 +79,34 @@ def run(args):
 def cli():
     parser = argparse.ArgumentParser(description='Manipulates a Figment world.')
 
-    parser.add_argument(
-        '-z', '--zone', type=str, default='default',
-        help='name of the target zone'
-    )
-    parser.add_argument(
-        '-w', '--world', type=str, default='',
-        help='path to the world'
-    )
+    parser.add_argument('-z', '--zone', type=str, default='default', help='name of the target zone')
+    parser.add_argument('-w', '--world', type=str, default='', help='path to the world')
 
-    subparsers = parser.add_subparsers(dest='command')
+    subparsers = parser.add_subparsers()
 
-    # New parser
+    def cmd(name, func, **kwargs):
+        p = subparsers.add_parser(name, **kwargs)
+        p.set_defaults(func=func)
+        p.arg = lambda *a, **k: p.add_argument(*a, **k) and p
+        return p
 
-    parser_new = subparsers.add_parser(
-        'new', help='create a new world'
-    )
-    parser_new.add_argument(
-        'name', type=str, help='world name (must be a valid directory name)'
-    )
-    parser_new.set_defaults(func=new)
+    cmd('new', new, help='create a new world')\
+        .arg('name', type=str, help='world name (must be a valid directory name)')
 
-    # Listen parser
+    cmd('listen', listen, help='connect to an entity\'s message stream')\
+        .arg('entity_id', type=str, help='ID of the target entity')
 
-    parser_listen = subparsers.add_parser(
-        'listen', help='connect to an entity\'s message stream'
-    )
-    parser_listen.add_argument(
-        'entity_id', type=str, help='ID of the target entity'
-    )
-    parser_listen.set_defaults(func=listen)
+    cmd('command', command, help='run a command from the entity\'s perspective')\
+        .arg('entity_id', type=str, help='ID of the target entity')\
+        .arg('command', type=str, help='the command to perform')
 
-    # Command parser
+    cmd('prompt', prompt, help='start an interactive prompt for performing commands')\
+        .arg('entity_id', type=str, help='ID of the target entity')
 
-    parser_command = subparsers.add_parser(
-        'command', help='run a command from the entity\'s perspective'
-    )
-    parser_command.add_argument(
-        'entity_id', type=str, help='ID of the target entity'
-    )
-    parser_command.add_argument(
-        'command', type=str, help='the command to perform'
-    )
-    parser_command.set_defaults(func=command)
-
-    # Prompt parser
-
-    parser_prompt = subparsers.add_parser(
-        'prompt', help='start an interactive prompt for performing commands'
-    )
-    parser_prompt.add_argument(
-        'entity_id', type=str, help='ID of the target entity'
-    )
-    parser_prompt.set_defaults(func=prompt)
-
-    # Run parser
-
-    parser_run = subparsers.add_parser('run', help='run a Figment zone server')
-    parser_run.add_argument(
-        '-v', '--verbose', action='store_true',
-        help='show verbose output'
-    )
-    parser_run.add_argument(
-        '-d', '--debug', action='store_true',
-        help='run pdb if Figment crashes'
-    )
-    parser_run.add_argument(
-        '-t', '--ticker', action='store_true',
-        help='run as a tick event generator'
-    )
-    parser_run.set_defaults(func=run)
+    cmd('run', run, help='run a Figment zone server')\
+        .arg('-v', '--verbose', action='store_true', help='show verbose output')\
+        .arg('-d', '--debug', action='store_true', help='run pdb if Figment crashes')\
+        .arg('-t', '--ticker', action='store_true', help='run as a tick event generator')
 
     args = parser.parse_args()
     args.func(args)
