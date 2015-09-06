e06f09f39ad8be91646f324bb8cba06a33fa036c
Jesse Dubay <jesse@jessedubay.com>
2015-09-06 16:21:36 -0700

    Add missing Important import

--- examples/the-high-street/components/__init__.py
+++ examples/the-high-street/components/__init__.py
@@ -4,4 +4,5 @@ from emotive import Emotive
 from psychic import Psychic
 from sticky import Sticky
 from fauna import Bird, Pest, ShoosPests
+from important import Important
 from wandering import Wandering
